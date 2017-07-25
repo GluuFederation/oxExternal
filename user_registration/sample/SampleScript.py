dbb0bc7297b0e73d07e364a24de771256c9f707b
rajnikant <rk@gluu.org>
2017-07-25 13:14:30 +0530

    implementation of post and confirm registration

--- user_registration/sample/SampleScript.py
+++ user_registration/sample/SampleScript.py
@@ -1,10 +1,11 @@
 # oxAuth is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
 # Copyright (c) 2016, Gluu
 #
-# Author: Yuriy Movchan
-#
 
+
+from org.xdi.service.cdi.util import CdiUtil
 from org.xdi.model.custom.script.type.user import UserRegistrationType
+from org.xdi.service import MailService
 from org.xdi.ldap.model import GluuStatus
 from org.xdi.util import StringHelper, ArrayHelper
 from java.util import Arrays, ArrayList
@@ -49,6 +50,7 @@ class UserRegistration(UserRegistrationType):
         if not self.enable_user:
             userStatus = GluuStatus.INACTIVE
 
+
         # Disable/Enable registered user
         user.setStatus(userStatus)
 
@@ -61,24 +63,18 @@ class UserRegistration(UserRegistrationType):
     def postRegistration(self, user, requestParameters, configurationAttributes):
         print "User registration. Post method"
 
+        mailService = CdiUtil.bean(MailService)
+        subject = "Confirmation mail for user registration"
+        body = "User Registered for %s. Please Confirm User Registration by clicking url: https://localhost:8453/identity/confirm/registration?code=%s" % (user.getMail(),user.getUserRandomKey())
+        mailService.sendMail(user.getMail(), subject, body)
         return True
-    
-    # User confirm New Registration method
-    #   user is org.gluu.oxtrust.model.GluuCustomPerson
-    #   requestParameters is java.util.Map<String, String[]>
-    #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
+
     def confirmRegistration(self, user, requestParameters, configurationAttributes):
-        print "User registration. confirm Registration method"
-
-        return True
-    
-    # User Get confirmation page method
-    #   user is org.gluu.oxtrust.model.GluuCustomPerson
-    #   requestParameters is java.util.Map<String, String[]>
-    #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
-    def getConfirmationPage(self, user, requestParameters, configurationAttributes):
-        print "User registration. Get confirmation page method"
-
+        print "User registration. Confirm method"
+        code = requestParameters.get("code")[0]
+        if code == user.getUserRandomKey():
+            user.setStatus(GluuStatus.ACTIVE)
+            self.enable_user = True
         return True
 
     def getApiVersion(self):
