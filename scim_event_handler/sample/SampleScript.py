664d5e906e18c338d0cf67c164d7b390db6fc7c1
Jose G <bonustrack310@gmail.com>
2018-02-19 10:47:57 -0500

    Add methods to script for https://github.com/GluuFederation/oxTrust/issues/845

--- scim_event_handler/sample/SampleScript.py
+++ scim_event_handler/sample/SampleScript.py
@@ -1,8 +1,9 @@
 # oxTrust is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
-# Copyright (c) 2014, Gluu
+# Copyright (c) 2018, Gluu
 #
 # Author: Val Pecaoco
-#
+# Updated by Jose Gonzalez on 2018-02-19
+
 from org.xdi.model.custom.script.type.scim import ScimType
 from org.xdi.util import StringHelper, ArrayHelper
 from java.util import Arrays, ArrayList
@@ -39,6 +40,9 @@ class ScimEventHandler(ScimType):
 
         return True
 
+    def postCreateUser(self, user, configurationAttributes):
+        return True
+
     def updateUser(self, user, configurationAttributes):
         personService = PersonService.instance()
         oldUser = personService.getPersonByUid(user.getUid())
@@ -46,18 +50,33 @@ class ScimEventHandler(ScimType):
         print "ScimEventHandler (updateUser): New displayName = " + user.getDisplayName()
         return True
 
+    def postUpdateUser(self, user, configurationAttributes):
+        return True
+
     def deleteUser(self, user, configurationAttributes):
         print "ScimEventHandler (deleteUser): Current id = " + user.getUid()
+        return True
+
+    def postDeleteUser(self, user, configurationAttributes):
         return True
 
     def createGroup(self, group, configurationAttributes):
         print "ScimEventHandler (createGroup): Current displayName = " + group.getDisplayName()
         return True
 
+    def postCreateGroup(self, group, configurationAttributes):
+        return True
+
     def updateGroup(self, group, configurationAttributes):
         print "ScimEventHandler (updateGroup): Current displayName = " + group.getDisplayName()
         return True
 
+    def postUpdateGroup(self, group, configurationAttributes):
+        return True
+
     def deleteGroup(self, group, configurationAttributes):
         print "ScimEventHandler (deleteGroup): Current displayName = " + group.getDisplayName()
+        return True
+
+    def postDeleteGroup(self, group, configurationAttributes):
         return True
