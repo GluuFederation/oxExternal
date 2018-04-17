0ded9755bf646db973906e1f6e987551c1abe117
yurem <Yuriy.Movchan@gmail.com>
2018-04-17 14:57:40 +0300

    Update scim script

--- scim_event_handler/sample/SampleScript.py
+++ scim_event_handler/sample/SampleScript.py
@@ -1,14 +1,13 @@
 # oxTrust is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
-# Copyright (c) 2018, Gluu
+# Copyright (c) 2014, Gluu
 #
-# Author: Val Pecaoco
-# Updated by Jose Gonzalez on 2018-02-19
-
+# Author: Jose Gonzalez
+#
 from org.xdi.model.custom.script.type.scim import ScimType
 from org.xdi.util import StringHelper, ArrayHelper
 from java.util import Arrays, ArrayList
-from org.xdi.service.cdi.util import CdiUtil
 from org.gluu.oxtrust.ldap.service import PersonService
+from org.xdi.service.cdi.util import CdiUtil
 from org.gluu.oxtrust.model import GluuCustomPerson
 
 import java
@@ -27,11 +26,9 @@ class ScimEventHandler(ScimType):
         return True   
 
     def getApiVersion(self):
-        #return 2 if you want the post* scripts getting called
+        #return 2 if you want the post* scripts being executed
         return 1
 
-    # user is an instance of org.gluu.oxtrust.model.GluuCustomPerson
-    # configurationAttributes is a Map<String, org.xdi.model.SimpleCustomProperty>
     def createUser(self, user, configurationAttributes):
 
         print "ScimEventHandler (createUser): Current id = " + user.getUid()
@@ -44,41 +41,42 @@ class ScimEventHandler(ScimType):
 
         return True
 
-    def postCreateUser(self, user, configurationAttributes):
-        return True
-
     def updateUser(self, user, configurationAttributes):
         personService = CdiUtil.bean(PersonService)
-        oneUser = personService.getPersonByUid(user.getUid())
-        print "ScimEventHandler (updateUser): user's displayName = " + oneUser.getDisplayName()
-        return True
-
-    def postUpdateUser(self, user, configurationAttributes):
+        oldUser = personService.getPersonByUid(user.getUid())
+        print "ScimEventHandler (updateUser): Old displayName %s" % oldUser.getDisplayName()
+        print "ScimEventHandler (updateUser): New displayName " + user.getDisplayName()
         return True
 
     def deleteUser(self, user, configurationAttributes):
         print "ScimEventHandler (deleteUser): Current id = " + user.getUid()
         return True
 
-    def postDeleteUser(self, user, configurationAttributes):
-        return True
-
     def createGroup(self, group, configurationAttributes):
         print "ScimEventHandler (createGroup): Current displayName = " + group.getDisplayName()
-        return True
-
-    def postCreateGroup(self, group, configurationAttributes):
         return True
 
     def updateGroup(self, group, configurationAttributes):
         print "ScimEventHandler (updateGroup): Current displayName = " + group.getDisplayName()
         return True
 
+    def deleteGroup(self, group, configurationAttributes):
+        print "ScimEventHandler (deleteGroup): Current displayName = " + group.getDisplayName()
+        return True
+        
+    def postCreateUser(self, user, configurationAttributes):
+        return True
+
+    def postUpdateUser(self, user, configurationAttributes):
+        return True
+
+    def postDeleteUser(self, user, configurationAttributes):
+        return True
+
     def postUpdateGroup(self, group, configurationAttributes):
         return True
 
-    def deleteGroup(self, group, configurationAttributes):
-        print "ScimEventHandler (deleteGroup): Current displayName = " + group.getDisplayName()
+    def postCreateGroup(self, group, configurationAttributes):
         return True
 
     def postDeleteGroup(self, group, configurationAttributes):
