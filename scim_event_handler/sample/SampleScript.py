0ef0f6421a8740e07f2e1714c5c31af4e41ca9f2
Jose <bonustrack310@gmail.com>
2021-07-30 11:48:56 -0500

    Update scim script

--- scim_event_handler/sample/SampleScript.py
+++ scim_event_handler/sample/SampleScript.py
@@ -1,11 +1,5 @@
+# Visit https://www.gluu.org/docs/gluu-server/user-management/scim-scripting/ to learn more
 from org.gluu.model.custom.script.type.scim import ScimType
-from org.gluu.util import StringHelper, ArrayHelper
-from java.util import Arrays, ArrayList
-from org.gluu.oxtrust.service import PersonService
-from org.gluu.service.cdi.util import CdiUtil
-from org.gluu.oxtrust.model import GluuCustomPerson
-#Comment the line below if you are using Gluu CE 3.x
-from org.gluu.oxtrust.model.scim import ScimCustomPerson
 
 import java
 
@@ -23,56 +17,30 @@ class ScimEventHandler(ScimType):
         return True   
 
     def getApiVersion(self):
-        #return 2 if you want the post* methods executed
-        #return 3 if you also want the get* methods executed
-        #return 4 if you also want the postSearch* methods executed
-        return 4
+        return 5
 
-    # user is an instance of ScimCustomPerson in CE 4.0 for all user-related methods
     def createUser(self, user, configurationAttributes):
-
-        print "ScimEventHandler (createUser): Current id = " + user.getUid()
-
-        testProp1 = configurationAttributes.get("testProp1").getValue2()
-        testProp2 = configurationAttributes.get("testProp2").getValue2()
-
-        print "ScimEventHandler (createUser): testProp1 = " + testProp1
-        print "ScimEventHandler (createUser): testProp2 = " + testProp2
-
         return True
 
     def updateUser(self, user, configurationAttributes):
-        personService = CdiUtil.bean(PersonService)
-        # oldUser is an instance of GluuCustomPerson
-        oldUser = personService.getPersonByUid(user.getUid())
-        print "ScimEventHandler (updateUser): Old displayName %s" % oldUser.getDisplayName()
-        print "ScimEventHandler (updateUser): New displayName %s" % user.getDisplayName()
         return True
 
     def deleteUser(self, user, configurationAttributes):
-        print "ScimEventHandler (deleteUser): Current id = " + user.getUid()
         return True
 
     def createGroup(self, group, configurationAttributes):
-        print "ScimEventHandler (createGroup): Current displayName = " + group.getDisplayName()
         return True
 
     def updateGroup(self, group, configurationAttributes):
-        print "ScimEventHandler (updateGroup): Current displayName = " + group.getDisplayName()
         return True
 
     def deleteGroup(self, group, configurationAttributes):
-        print "ScimEventHandler (deleteGroup): Current displayName = " + group.getDisplayName()
         return True
         
     def postCreateUser(self, user, configurationAttributes):
-    	# user is the instance as modified in createUser method. Modifications applied here will not
-    	# take effect in the output of the API and will not be persisted automatically to database either 
         return True
 
     def postUpdateUser(self, user, configurationAttributes):
-    	# user is the instance as modified in updateUser method. Modifications applied here will not
-    	# take effect in the output of the API and will not be persisted automatically to database either
         return True
 
     def postDeleteUser(self, user, configurationAttributes):
@@ -87,24 +55,32 @@ class ScimEventHandler(ScimType):
     def postDeleteGroup(self, group, configurationAttributes):
         return True
     
-    #This method is available in CE 4.1 onwards
     def getUser(self, user, configurationAttributes):
         return True
     
-    #This method is available in CE 4.1 onwards
     def getGroup(self, group, configurationAttributes):
         return True
-
-	# This method is available in CE 4.2 onwards
-	# results is an instance of org.gluu.persist.model.PagedResult<ScimCustomPerson>
+        
     def postSearchUsers(self, results, configurationAttributes):
-    	# person = results.getEntries().get(0)
-    	# person.setUid("fake_uid")
-    	# Modifications on results variable will take effect on the output of the API call
-    	return True
+        # Warning: postSearchUsers is a misnomer. This gets actually executed before the 
+        # SCIM search operation is serialized, so modifications on results variable will
+        # take effect on the output of the API call
+        return True
 
-	# This method is available in CE 4.2 onwards
-	# results is an instance of org.gluu.persist.model.PagedResult<org.gluu.oxtrust.model.GluuGroup>
     def postSearchGroups(self, results, configurationAttributes):
-    	# Modifications on results variable will take effect on the output of the API call
-    	return True
+        # Warning: postSearchGroups is a misnomer. This gets actually executed before the 
+        # SCIM search operation is serialized, so modifications on results variable will
+        # take effect on the output of the API call
+        return True
+        
+    def allowResourceOperation(self, context, entity, configurationAttributes):
+        return True 
+    
+    def allowSearchOperation(self, context, configurationAttributes):
+        return ""
+    
+    def rejectedResourceOperationResponse(self, context, entity, configurationAttributes):
+        return None       
+    
+    def rejectedSearchOperationResponse(self, context, configurationAttributes):
+        return None
